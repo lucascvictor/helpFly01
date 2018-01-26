660c3f5a4d4b1df97225e4f30b7c077ad087ebeb
lukevictor <lucas.c.victor@hotmail.com>
2018-01-26 18:11:39 -0200

    Correções do menu

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -18,12 +18,12 @@
         <a class="collapsible-header">Financeiro<i class="material-icons expand right">expand_more</i></a>
         <div class="collapsible-body" style="display: block;">
           <ul>
-            <li class=""><a href="{{ url('/produtos') }}" onclick="fly01.go(&quot;/Json/OrderNFe&quot;)">Fluxo de Caixa</a></li>
-            <li class=""><a href="javascript:void(0)" onclick="fly01.go(&quot;/Json/OrderNFSe&quot;)">Extrato</a></li>
-            <li class=""><a href="javascript:void(0)" onclick="fly01.go(&quot;/Json/OrderNFSe&quot;)">Contas a Pagar</a></li>
-            <li class=""><a href="javascript:void(0)" onclick="fly01.go(&quot;/Json/OrderNFSe&quot;)">Contas a Receber</a></li>
-            <li class=""><a href="javascript:void(0)" onclick="fly01.go(&quot;/Json/OrderNFSe&quot;)">Relatório DRE</a></li>
-            <li class=""><a href="javascript:void(0)" onclick="fly01.go(&quot;/Json/OrderNFSe&quot;)">Conciliação Bancária</a></li>
+            <li class=""><a href="{{ url('/produtos') }}" >Fluxo de Caixa</a></li>
+            <li class=""><a href="#" >Extrato</a></li>
+            <li class=""><a href="#" >Contas a Pagar</a></li>
+            <li class=""><a href="#" >Contas a Receber</a></li>
+            <li class=""><a href="#" >Relatório DRE</a></li>
+            <li class=""><a href="#" >Conciliação Bancária</a></li>
                         
           </ul>
         </div>
@@ -35,12 +35,12 @@
       <li class=""><a class="collapsible-header">Cadastros<i class="material-icons expand right">expand_more</i></a>
         <div class="collapsible-body" style="display: none;">
           <ul>
-            <li class=""><a href="{{ url('/cadastros/clientes') }}" onclick="fly01.go(&quot;/Json/RegisterPerson&quot;)">Clientes</a></li>
-            <li class=""><a href="{{ url('/cadastros/fornecedores') }}" onclick="fly01.go(&quot;/Json/RegisterSupplier&quot;)">Fornecedores</a></li>
-            <li class=""><a href="{{ url('/cadastros/grupostributarios') }}" onclick="fly01.go(&quot;/Json/RegisterTaxGroup&quot;)">Concições de Parcelamento</a></li>
-            <li class=""><a href="{{ url('/cadastros/produtos') }}" onclick="fly01.go(&quot;/Json/RegisterProduct&quot;)">Categorias Financeiras</a></li>
-            <li class=""><a href="{{ url('/cadastros/gruposdeprodutos') }}" onclick="fly01.go(&quot;/Json/GroupProduct&quot;)">Formas de Pagamento</a></li>
-            <li class=""><a href="{{ url('/cadastros/servicos') }}" onclick="fly01.go(&quot;/Json/RegisterService&quot;)">Contas Bancárias</a></li>
+            <li class=""><a href="{{ url('/cadastros/clientes') }}" >Clientes</a></li>
+            <li class=""><a href="{{ url('/cadastros/fornecedores') }}" >Fornecedores</a></li>
+            <li class=""><a href="{{ url('/cadastros/grupostributarios') }}" >Concições de Parcelamento</a></li>
+            <li class=""><a href="{{ url('/cadastros/produtos') }}" >Categorias Financeiras</a></li>
+            <li class=""><a href="{{ url('/cadastros/gruposdeprodutos') }}" >Formas de Pagamento</a></li>
+            <li class=""><a href="{{ url('/cadastros/servicos') }}" >Contas Bancárias</a></li>
           </ul>
         </div>
       </li>
@@ -48,7 +48,7 @@
   </li>
   <li class="no-padding"><ul class="collapsible collapsible-accordion"><li><a class="collapsible-header">Ajuda<i class="material-icons expand right">expand_more</i></a>
     <div class="collapsible-body"><ul>
-            <li class=""><a href="javascript:void(0)" onclick="fly01.go(&quot;https://secure.logmeinrescue.com/customer/code.aspx&quot;)">AssistÃªncia Remota</a>
+            <li class=""><a href="#" onclick="fly01.go(&quot;https://secure.logmeinrescue.com/customer/code.aspx&quot;)">AssistÃªncia Remota</a>
             </li>
           </ul>
         </div>
