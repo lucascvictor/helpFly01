d1dced16440066a5a2baf91c9b9798ffd631f5a4
lukevictor <lucas.c.victor@hotmail.com>
2018-02-05 08:35:19 -0200

    Alterações no menu para não redirecionar

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -3,22 +3,22 @@
     <div class="nav-wrapper">
       <a id="page-title" class="page-title">Financeiro</a>
       <ul class="right">
-        <li><a class="dropdown-button" href="https://novofaturamento.fly01.com.br/#!" data-activates="user-menu" data-beloworigin="true" data-alignment="right" data-vivaldi-spatnav-clickable="1"><i class="material-icons  ">perm_identity</i></a>
+        <li><a class="dropdown-button" href="" data-activates="user-menu" data-beloworigin="true" data-alignment="right" data-vivaldi-spatnav-clickable="1"><i class="material-icons  ">perm_identity</i></a>
           <ul id="user-menu" class="dropdown-content">
             <li class="">
-            <a href="javascript:void(0)" onclick="fly01.go(&quot;/Json/FormRegisterCompany&quot;,&quot;01&quot;)">Empresa</a></li>
+            <a href="" >Empresa</a></li>
             <li class="">
-              <a href="javascript:void(0)" onclick="fly01.go(&quot;/Json/FormParametros&quot;,&quot;01&quot;)">Parâmetros</a></li>
+              <a href="">Parâmetros</a></li>
           <li class="">
-            <a href="javascript:void(0)" onclick="fly01.go(&quot;/Json/FormRegisterCertificate&quot;,&quot;01&quot;)">Certificado</a>
+            <a href="">Certificado</a>
           </li>
           <li class="">
-            <a href="javascript:void(0)" onclick="fly01.go(&quot;/Json/RegisterInvoiceSeries&quot;)">Série NF</a>
+            <a href="">Série NF</a>
           </li>
           <li class="divider">
           </li>
           <li class="">
-            <a href="/Account/Logoff">Sair</a>
+            <a href="{{ url('/Account/Logoff') }}">Sair</a>
           </li>
         </ul>
       </li>
