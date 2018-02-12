5d402c8fc59b1c3eeee719fece09bd6c64d3a2e3
lukevictor <lucas.c.victor@hotmail.com>
2018-02-12 11:01:29 -0200

    Teste index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,72 @@
 @section('content')
 
 
+<div class="row">
+        <div id="page-wrapper">
+            
+
+
+
+
+<div class="panel panel-default">
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-lg-10 col-lg-offset-1 col-xs-12">
+                    <div class="col-md-4 col-xs-6 installedApp">
+                        <div class="fmcircle_out">
+                            <a href="https://novofinanceiro.fly01.com.br">
+                                <div class="fmcircle_border">
+                                    <div class="fmcircle_in fmcircle_orange">
+                                        <span>Financeiro (Novo)</span><img src="http://187.94.62.174/AuthServer/Images/AppImages/9431cdb8_image.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-xs-6 installedApp">
+                        <div class="fmcircle_out">
+                            <a href="https://novofaturamento.fly01.com.br">
+                                <div class="fmcircle_border">
+                                    <div class="fmcircle_in fmcircle_orange">
+                                        <span>Faturamento (Novo)</span><img src="http://187.94.62.174/AuthServer/Images/AppImages/dfc2cdaa_image.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-xs-6 installedApp">
+                        <div class="fmcircle_out">
+                            <a href="https://novocompras.fly01.com.br">
+                                <div class="fmcircle_border">
+                                    <div class="fmcircle_in fmcircle_orange">
+                                        <span>Compras (Novo)</span><img src="http://187.94.62.174/AuthServer/Images/AppImages/a8e5f1ce_image.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-xs-6 installedApp">
+                        <div class="fmcircle_out">
+                            <a href="https://novoestoque.fly01.com.br">
+                                <div class="fmcircle_border">
+                                    <div class="fmcircle_in fmcircle_orange">
+                                        <span>Estoque (Novo)</span><img src="http://187.94.62.174/AuthServer/Images/AppImages/79579272_image.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+        </div>
+    </div>
+
+
+
+
 <div class="fixed-action-btn horizontal">
     <a class="btn-floating btn-large orange" style="width: 100px; height: 100px;">
       Acesse o helpFly
