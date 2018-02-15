920e300dad38a92392d53c060b55ee8a57a0f843
lukevictor <lucas.c.victor@hotmail.com>
2018-02-15 12:51:09 -0200

    Inclusão dos requisitos para utilização do enjoyhint

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,5 +1,6 @@
   <script src="https://cdnfly01.azureedge.net/fly/1.0.2/fly01ui.js"></script>
   <script src="{{ url('js/fly-tour.js') }}"></script>
+  <script src="{{ url('js/enjoyhint.js') }}"></script>
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -2,6 +2,8 @@
     <link href="https://cdnfly01.azureedge.net/fly/1.0.2/fly01ui.css" rel="stylesheet">
 
     <link href="{{ url('font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+
+    <link href="{{ url('css/enjoyhint.css') }}" rel="stylesheet" type="text/css">
        <!--Let browser know website is optimized for mobile-->
      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
