a4d9a0db38ac75b85ea752f6d0c6e98eff002561
lukevictor <lucas.c.victor@hotmail.com>
2018-02-20 13:57:42 -0300

    Correções de tour

--- resources/views/cadastros/formas_de_pagamento/tour.php
+++ resources/views/cadastros/formas_de_pagamento/tour.php
@@ -12,24 +12,20 @@
       }
        {
           
-          selector:'#tipoFormaPagamentoField',
-          event:'click',
-          description:'No campo “Tipo” temos a opção de selecionar a forma do pagamento a qual vamos selecionar, basta clicar em cima e ver as opções.',
+          'next #tipoFormaPagamentoField': 'No campo “Tipo” temos a opção de selecionar a forma do pagamento a qual vamos selecionar, basta clicar em cima e ver as opções.',
           'nextButton' : {className: 'myNext', text: 'Próximo'},
           'skipButton' : {className: 'mySkip', text: 'Sair'},
-          timeout:100
+
         },
         {
-          selector:'#descricaoField',
-          event:'click',
-          description:'Em “Descrição” preenchemos a que se referencia aquela forma. Ex: Caso você selecione o Tipo “Transferência”, em descrição é possível especificar a qual conta foi feita o pagamento.',
-  
+          'next #descricaoField':'Em “Descrição” preenchemos a que se referencia aquela forma. Ex: Caso você selecione o Tipo “Transferência”, em descrição é possível especificar a qual conta foi feita o pagamento.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
         },
         {
-          selector:'#fly01frm',
-          event:'click',
-          description:'**ATENÇÃO** É importante ter um bom controle das formas de pagamentos, pois elas serão sempre utilizadas na inclusão de títulos financeiros no sistema.',
-  
+          'next #fly01frm': '**ATENÇÃO** É importante ter um bom controle das formas de pagamentos, pois elas serão sempre utilizadas na inclusão de títulos financeiros no sistema.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
         },
         
       ];
