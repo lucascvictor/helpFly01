c38466d8afed4216939f32fca1aeab2d33b93040
lukevictor <lucas.c.victor@hotmail.com>
2018-02-20 09:02:08 -0300

    Remoção do inicio automático do tour

--- resources/views/financeiro/fluxo_de_caixa/tour.blade.php
+++ resources/views/financeiro/fluxo_de_caixa/tour.blade.php
@@ -66,11 +66,11 @@
       ];
 
       
-      var enjoyhint_instance = null;
-      $(document).ready(function(){
+        var enjoyhint_instance = null;
+      
         enjoyhint_instance = new EnjoyHint({});
         enjoyhint_instance.setScript(enjoyhint_script_data);
-        enjoyhint_instance.runScript();
-      });
+        
+    
 
 	</script>
