e68611801ef32ecdc8f8d1f68facc94a1000bc7d
Felipe Canova <fc.lavagnoli@gmail.com>
2018-02-22 17:26:41 -0300

    Inicio Tour Cond. de Parcelamento

---
+++ resources/views/cadastros/condicoes_de_parcelamento/tour.blade.php
@@ -0,0 +1,46 @@
+ 
+ <script>
+	   localStorage.clear();
+      var enjoyhint_script_data = [
+       {
+          'next #descricao': 'Preencha o nome da condição a qual você vai criar',
+          'top': -20,
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+
+        },
+
+        {
+          'next #descricao': 'Lembre-se que este é o nome da condição, você irá sempre referencia-la nas criações de títulos financeiro.',
+          'top': -20,
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+
+        },
+
+
+        {
+          'next #descricaoField':'Em “Descrição” preenchemos a que se referencia aquela forma. Ex: Caso você selecione o Tipo “Transferência”, em descrição é possível especificar a qual conta foi feita o pagamento.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+        {
+          'next #salvar':'Com ambos os dados inseridos basta apenas clicar em “SALVAR”.”, em descrição é possível especificar a qual conta foi feita o pagamento.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+
+        {
+          'next #fly01frm': '**ATENÇÃO** É importante ter um bom controle das formas de pagamentos, pois elas serão sempre utilizadas na inclusão de títulos financeiros no sistema.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+
+       {
+          'next #fly01frm': 'Restou alguma dúvida? Mande um e-mail para suporte.fly01gestao@totvs.com.br e iremos respondê-la para você.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+        
+      ];
+	</script>
