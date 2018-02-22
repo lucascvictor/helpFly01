1a7d8dcfbb927be51d7f27407f4342480c4c1fe3
Felipe Canova <fc.lavagnoli@gmail.com>
2018-02-22 18:09:02 -0300

    Inicio Tour Cond. de Parcelamento

--- resources/views/cadastros/condicoes_de_parcelamento/tour.blade.php
+++ resources/views/cadastros/condicoes_de_parcelamento/tour.blade.php
@@ -20,23 +20,66 @@
 
 
         {
-          'next #descricaoField':'Em “Descrição” preenchemos a que se referencia aquela forma. Ex: Caso você selecione o Tipo “Transferência”, em descrição é possível especificar a qual conta foi feita o pagamento.',
+          'next #TipoSimulacaoField':'É neste campo onde é definido qual o tipo de condição você deseja inserir.',
           'nextButton' : {className: 'myNext', text: 'Próximo'},
           'skipButton' : {className: 'mySkip', text: 'Sair'},
         },
+        
         {
-          'next #salvar':'Com ambos os dados inseridos basta apenas clicar em “SALVAR”.”, em descrição é possível especificar a qual conta foi feita o pagamento.',
+          'next #TipoSimulacaoField':'De forma mais precisa, o Fly01 Gestão oferece dois tipos de condições, por quantidade de parcelas, ou por intervalo de dias',
           'nextButton' : {className: 'myNext', text: 'Próximo'},
           'skipButton' : {className: 'mySkip', text: 'Sair'},
         },
 
         {
-          'next #fly01frm': '**ATENÇÃO** É importante ter um bom controle das formas de pagamentos, pois elas serão sempre utilizadas na inclusão de títulos financeiros no sistema.',
+          'next #qtdParcelasField':'Caso você escolha "Quantidade de Parcelas, este campo será habilitado e nele você irá informar a quantidade de parcelas que seu título irá possuir.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+
+        {
+          'next #qtdParcelasField': 'Vale ressaltar que esta opção irá fazer com que as parcelas sejam feitas com um intervalo fixo de 30 dias, caso queira um controle mais específico a opção "Cond. Parcelamento"',
           'nextButton' : {className: 'myNext', text: 'Próximo'},
           'skipButton' : {className: 'mySkip', text: 'Sair'},
         },
 
        {
+          'next #condicoesParcelamentoField': 'Se a opção "Cond. Parcelamento for selecionada, este campo será habilitado, nele selecionamos o período das parcelas preenchendo o intervalo de dias como condição(Ex: 0,10,20,30).',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+
+        {
+          'next #dadosCondParc':'Com todos os campos preenchidos, já é possível salvar sua condição, entretanto, antes de salvar a condição, é possível realizar a simulação desta condição.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+
+           {
+          'next #fly01frm': '',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+
+        {
+          'next #fly01frm': '',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+
+           {
+          'next #fly01frm': '',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+
+           {
+          'next #fly01frm': '',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },
+
+        {
           'next #fly01frm': 'Restou alguma dúvida? Mande um e-mail para suporte.fly01gestao@totvs.com.br e iremos respondê-la para você.',
           'nextButton' : {className: 'myNext', text: 'Próximo'},
           'skipButton' : {className: 'mySkip', text: 'Sair'},
