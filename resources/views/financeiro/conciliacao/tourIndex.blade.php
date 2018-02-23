be55445a95372c230a434de726b62f37c00db21f
Felipe Canova <fc.lavagnoli@gmail.com>
2018-02-23 13:08:09 -0300

    Botão de tour

---
+++ resources/views/financeiro/conciliacao/tourIndex.blade.php
@@ -0,0 +1,17 @@
+ <script>
+	   localStorage.clear();
+      var enjoyhint_script_data = [
+       {
+          'next #container': 'Antes de iniciarmos é necessário saber que é preciso ter suas contas bancárias previamente cadastradas no sistema e elas devem ser correspondentes com o extrato do qual você está realizando a importação. <clique aqui> caso você não saibacadastrar contas bancárias.',
+          'top': -20,
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+
+        },
+        {
+          'next #new': 'Vá em “NOVO”',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+        },        
+      ];
+	</script>
