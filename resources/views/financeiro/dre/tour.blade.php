e4b45904b6954702dc85e073d01c6680e6678556
lukevictor <lucas.c.victor@hotmail.com>
2018-02-26 08:38:23 -0300

    Tour de DRE

---
+++ resources/views/financeiro/dre/tour.blade.php
@@ -0,0 +1,21 @@
+
+ <script type="text/javascript">
+	  localStorage.clear();
+
+      var enjoyhint_script_data = [
+        {
+          'next .fly01areatotal': 'Antes de iniciarmos é necessário saber que é preciso ter suas contas bancárias previamente cadastradas no sistema e elas devem ser correspondentes com o extrato do qual você está realizando a importação. <clique aqui> caso você não saiba cadastrar contas bancárias.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+  
+        },
+        
+        {
+          'next #dataInicialField': 'Vá em  novo.',
+          'nextButton' : {className: 'myNext', text: 'Próximo'},
+          'skipButton' : {className: 'mySkip', text: 'Sair'},
+
+        },
+      ];
+      
+	</script>
