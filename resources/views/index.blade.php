4671d2056a0db026a8ba74a7adacbc62272108fa
lukevictor <lucas.c.victor@hotmail.com>
2018-03-05 18:44:20 -0300

    Alteração do bloco da pagina inicial e lista de videos do canal do youtube

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,105 +1,83 @@
 @extends('layouts.default')
-
 @section('title', 'Help Fly01')
-
 @section('content')
 <div id="headTop" class="z-depth-0-half">
-      <div class="container">
-         <div class="row">
-            <div class="col s8 fly01-main-header">
-               <h4 class="thin inline-block">Index</h4>
-               <ul class="right valign-wrapper fly01-buttons">
-                  
-               </ul>
-            </div>
+   <div class="container">
+      <div class="row">
+         <div class="col s8 fly01-main-header">
+            <h4 class="thin inline-block">Index</h4>
+            <ul class="right valign-wrapper fly01-buttons">
+            </ul>
          </div>
       </div>
    </div>
-   <div class="container">
-      <div class="row">
-
-    <div class="areageral" style="heigth: 300px;">
-    <div class="card orange accent-3">
-                  <div class="s8 m2">
-                        <div class="card-content white-text">
-                            <p class="bounceEffect animated bounceIn">
-                           <span>texto sobre finanças<i class="material-icons">fingerprint</i></span>
-                            </p>
-                        </div>
-                  </div>
-                  <div class="s8 m2">
-                    </div><div class="card orange accent-3">
-                        <div class="card-content white-text">
-                            <p class="bounceEffect animated bounceIn">
-                            <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                            </p>
-                        </div>
-                  </div>
-                  <div class="s8 m2">
-                        <div class="card orange accent-3">
-                        <div class="card-content white-text">
-                            <p class="bounceEffect animated bounceIn">
-                            <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                            </p>
-                        </div>
-                    </div>
-                    </div>
-		 </div>
-
-
-    <div class="col m2" style="width: 20%; heigth: 100%">
- 
-
-    <div class="areageral" style="heigth: 300px;">
-       
-    <div class="areageral" style="heigth: 300px;">
-      
-                    <div class="card orange accent-3">
-                        <div class="card-content white-text">
-                            <p class="bounceEffect animated bounceIn">
-                            <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                            </p>
-                        </div>
-                    </div>
-    
-
-
-                    <div class="card orange accent-3">
-                        <div class="card-content white-text">
-                            <p class="bounceEffect animated bounceIn">
-                            <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="card orange accent-3">
-                        <div class="card-content white-text">
-                            <p class="bounceEffect animated bounceIn">
-                            <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                            </p>
-                        </div>
-                    </div>
- 
+</div>
+<div class="container">
+<div class="row">
+<div class="areageral" style="heigth: 300px;">
+<div class="card orange accent-3">
+   <div class="s8 m2">
+      <div class="card-content white-text">
+         <p class="bounceEffect animated bounceIn">
+            <span>texto sobre finanças<i class="material-icons">fingerprint</i></span>
+         </p>
       </div>
-            </div>
-  
-          
-
- 
+   </div>
+   <div class="s8 m2">
+   </div>
+   <div class="card orange accent-3">
+      <div class="card-content white-text">
+         <p class="bounceEffect animated bounceIn">
+            <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+         </p>
+      </div>
+   </div>
+   <div class="s8 m2">
+      <div class="card orange accent-3">
+         <div class="card-content white-text">
+            <p class="bounceEffect animated bounceIn">
+               <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </p>
+         </div>
+      </div>
+   </div>
+</div>
+<div class="col m2" style="width: 20%; heigth: 100%">
+<div class="areageral" style="heigth: 300px;">
+   <div class="areageral" style="heigth: 300px;">
+      <div class="card orange accent-3">
+         <div class="card-content white-text">
+            <p class="bounceEffect animated bounceIn">
+               <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </p>
+         </div>
+      </div>
+      <div class="card orange accent-3">
+         <div class="card-content white-text">
+            <p class="bounceEffect animated bounceIn">
+               <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </p>
+         </div>
+      </div>
+      <div class="card orange accent-3">
+         <div class="card-content white-text">
+            <p class="bounceEffect animated bounceIn">
+               <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </p>
+         </div>
+      </div>
+   </div>
+</div>
 @endsection
-
-
 @section('footer')
-
- <footer class="page-footer black">
-       
-       <div class="footer-copyright"> 
-       <p> USD: {{ $valores['valores']['USD']['valor'] }} </p> &nbsp&nbsp
-       <p> EUR: {{ $valores['valores']['EUR']['valor'] }} </p> &nbsp&nbsp
-       <p> BTC: {{ $valores['valores']['BTC']['valor'] }} </p> &nbsp&nbsp
-       </div>
+<footer class="page-footer black">
+   <div class="footer-copyright">
+      <p> USD: {{ $valores['valores']['USD']['valor'] }} </p>
+      &nbsp&nbsp
+      <p> EUR: {{ $valores['valores']['EUR']['valor'] }} </p>
+      &nbsp&nbsp
+      <p> BTC: {{ $valores['valores']['BTC']['valor'] }} </p>
+      &nbsp&nbsp
+   </div>
 </footer>
-
 @endsection
-
-
