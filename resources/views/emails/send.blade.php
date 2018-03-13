2004416d99b2f6e6a672a89a6d085723fba1a900
lucas <lucas.c.victor@hotmail.com>
2018-03-13 13:34:56 -0300

    modelo de envio do email

---
+++ resources/views/emails/send.blade.php
@@ -0,0 +1,7 @@
+<p> Nome: {{ $nomeCompleto }} </p>
+<p> Nome Empresa: {{ $nomeEmpresa }}</p>
+<p> Documento: {{ $nmrDocumento }}</p>
+<p> Telefone: {{ $telefone }}</p>
+<p> Assunto: {{ $assunto }}</p>
+<p> Mensagem: {{ $textarea1 }}</p>
+<p> Data: {{ $data }}</p>
