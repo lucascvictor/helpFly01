0424e029ee3cc6ecd0bdd1a0c6307cb1d9333d04
Lucas <lucas.c.victor@hotmail.com>
2018-03-27 13:12:11 -0300

    Fianlização do mapeamento de person

--- database/migrations/2018_03_27_120533_create_person_table.php
+++ database/migrations/2018_03_27_120533_create_person_table.php
@@ -18,12 +18,29 @@ class CreatePersonTable extends Migration
             $table->string('document')->unique();
             $table->string('name');
             $table->string('traddingName');
-            $table->integer('typeField')->unsigned();
+            $table->integer('type')->unsigned();
             $table->string('stateInscription');
             $table->bool('chkIsento');
             $table->integer('personTypeNfseField')->unsigned();
-            
-            
+            $table->string('suframal');
+            $table->string('municipalInscription');
+            $table->bool('simplesNacionalBool');
+            $table->string('otherCityAutoComplete');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('contato');
+            $table->string('celPhone');
+            $table->string('zipCode');
+            $table->string('stateField');
+            $table->string('cityDescricao');
+            $table->string('neighborhood');
+            $table->string('adress');
+            $table->string('observationField');
+            $table->bool('isSupplierBool');   
+            $table->bool('isCarrierBool');
+            $table->bool('isCustomerBool');
+            $table->bool('isSalesManBool');
+
             $table->rememberToken();
             $table->timestamps();
         });
