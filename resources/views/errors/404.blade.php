77f8386e48ff525d28786f3e5be35514d0e116e3
Felipe Canova <fc.lavagnoli@gmail.com>
2018-03-29 11:47:02 -0300

    Pagina Erro404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -19,23 +19,28 @@
                 font-family: 'Lato';
             }
 
+            #erro404 {
+                text-shadow: 3px -2px #263238;
+            }
 
         </style>
           @include('partials.styles')
     </head>
-    <body style="background-color: #ADCEDD;">
+    <body style="background-color: #37474f ;">
         <div class="container">
         <center>
-        <p>
+        <p id="erro404">
                 <b><span style="color: #FFF; font-size: 200px; ">4</span></b>
                 <b><span style="color: #FFF; font-size: 200px; ">0</span></b>
                 <b><span style="color: #FFF; font-size: 200px; ">4</span></b>
         </p>
         <p>
-                <h1><span style="color: #FFF; ">Pagina não encontrada :( .</span></h1>
-                <h2><span style="color: #FFF; ">Por favor entre em contato com o Administrador</p></span></h2>
+                <h3><b><span style="color: #FFF; ">Pagina não encontrada :(</span></b></h3>
+                <h4><b><span style="color: #FFF; ">Por favor entre em contato com o Suporte</p></span></b></h4>
 
         </p>
+        <a href="/" class="btn green darken-3 "><b>Home</b></a>
+        <a href="suporte" class="btn green darken-3"><b>Entrar em Contato</b></a>
         </center>
         </div>
     </body>
