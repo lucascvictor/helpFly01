0b47e43b8b76bc44e388d50a86d61cfc0226fcb6
Lucas <lucas.c.victor@hotmail.com>
2018-03-29 09:20:06 -0300

    Formatacao de tags

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,3 @@
-
-
 @extends('layouts.default')
 @section('title', 'Help Fly01')
 @section('content')
@@ -16,136 +14,145 @@
 </div>
 <div class="container ">
    <div class="row">
-       <div class="col s12 m4">
-      <div class="app" id="acc8b796-04a0-4f4f-b6a4-924d1ca4c27b"><div class="app-content white-text" style="background:#c39d00;"><img class="app-icon loaded" src="https://cdnfly01.blob.core.windows.net/img/icon/financeiro.png" onload="$(this).addClass('loaded')"><span class="app-title center"> Fly01 Financeiro</span></div></div>
-    </div>
-     <div class="col s12 m4">
-      <div class="app" id="acc8b796-04a0-4f4f-b6a4-924d1ca4c27b"><div class="app-content white-text" style="background:#c39d00;"><img class="app-icon loaded" src="https://cdnfly01.blob.core.windows.net/img/icon/financeiro.png" onload="$(this).addClass('loaded')"><span class="app-title center"> Fly01 Financeiro</span></div></div>
-    </div>
-     <div class="col s12 m4">
-      <div class="app" id="acc8b796-04a0-4f4f-b6a4-924d1ca4c27b"><div class="app-content white-text" style="background:#c39d00;"><img class="app-icon loaded" src="https://cdnfly01.blob.core.windows.net/img/icon/financeiro.png" onload="$(this).addClass('loaded')"><span class="app-title center"> Fly01 Financeiro</span></div></div>
-    </div>
-    
-
-       <div class = "col s4 m4">
-            <div class = "card blue-orange lighten-4">
-               <div class = "card-image">
-                  <img src = "http://blog.safetec.com.br/wp-content/uploads/2017/03/como-fazer-planejamento-financeiro-1.jpg">                
-               </div>
-               
-               <div class = "card-content">                  
-                  <p>HTML5 is the next major revision of the HTML standard superseding
-                     HTML 4.01, XHTML 1.0, and XHTML 1.1. HTML5 is a standard for
-                     structuring and presenting content on the World Wide Web.</p>
-               </div>
-               
-               <div class = "card-action">
-                  <button class = "btn waves-effect waves-light blue-grey">
-                     <i class = "material-icons">share</i></button>
-                  <a class = "right blue-grey-text" href = "http://www.tutorialspoint.com">
-                     www.tutorialspoint.com</a>
-               </div>
-            </div>
-      </div>
-      <div class = "col s4 m4">
-            <div class = "card blue-orange lighten-4">
-               <div class = "card-image">
-                  <img src = "http://blog.safetec.com.br/wp-content/uploads/2017/03/como-fazer-planejamento-financeiro-1.jpg">                
-               </div>
-               
-               <div class = "card-content">                  
-                  <p>HTML5 is the next major revision of the HTML standard superseding
-                     HTML 4.01, XHTML 1.0, and XHTML 1.1. HTML5 is a standard for
-                     structuring and presenting content on the World Wide Web.</p>
-               </div>
-               
-               <div class = "card-action">
-                  <button class = "btn waves-effect waves-light blue-grey">
-                     <i class = "material-icons">share</i></button>
-                  <a class = "right blue-grey-text" href = "http://www.tutorialspoint.com">
-                     www.tutorialspoint.com</a>
-               </div>
-            </div>
-      </div>
-      <div class = "col s4 m4">
-            <div class = "card blue-orange lighten-4">
-               <div class = "card-image">
-                  <img src = "http://blog.safetec.com.br/wp-content/uploads/2017/03/como-fazer-planejamento-financeiro-1.jpg">                
-               </div>
-               
-               <div class = "card-content">                  
-                  <p>HTML5 is the next major revision of the HTML standard superseding
-                     HTML 4.01, XHTML 1.0, and XHTML 1.1. HTML5 is a standard for
-                     structuring and presenting content on the World Wide Web.</p>
-               </div>
-               
-               <div class = "card-action">
-                  <button class = "btn waves-effect waves-light blue-grey">
-                     <i class = "material-icons">share</i></button>
-                  <a class = "right blue-grey-text" href = "http://www.tutorialspoint.com">
-                     www.tutorialspoint.com</a>
-               </div>
-            </div>
-      </div>
-  
-
-
-
-
-     
-
       <div class="col s12 m4">
-        <div class="card-content white-text">
-     
-  <div class="card orange accent-3">
+         <div class="app" id="acc8b796-04a0-4f4f-b6a4-924d1ca4c27b">
+            <div class="app-content white-text" style="background:#c39d00;"><img class="app-icon loaded" src="https://cdnfly01.blob.core.windows.net/img/icon/financeiro.png" onload="$(this).addClass('loaded')"><span class="app-title center"> Fly01 Financeiro</span></div>
+         </div>
+      </div>
+      <div class="col s12 m4">
+         <div class="app" id="acc8b796-04a0-4f4f-b6a4-924d1ca4c27b">
+            <div class="app-content white-text" style="background:#c39d00;"><img class="app-icon loaded" src="https://cdnfly01.blob.core.windows.net/img/icon/financeiro.png" onload="$(this).addClass('loaded')"><span class="app-title center"> Fly01 Financeiro</span></div>
+         </div>
+      </div>
+      <div class="col s12 m4">
+         <div class="app" id="acc8b796-04a0-4f4f-b6a4-924d1ca4c27b">
+            <div class="app-content white-text" style="background:#c39d00;"><img class="app-icon loaded" src="https://cdnfly01.blob.core.windows.net/img/icon/financeiro.png" onload="$(this).addClass('loaded')"><span class="app-title center"> Fly01 Financeiro</span></div>
+         </div>
+      </div>
+      <div class = "col s4 m4">
+         <div class = "card blue-orange lighten-4">
+            <div class = "card-image">
+               <img src = "http://blog.safetec.com.br/wp-content/uploads/2017/03/como-fazer-planejamento-financeiro-1.jpg">                
+            </div>
+            <div class = "card-content">
+               <p>HTML5 is the next major revision of the HTML standard superseding
+                  HTML 4.01, XHTML 1.0, and XHTML 1.1. HTML5 is a standard for
+                  structuring and presenting content on the World Wide Web.
+               </p>
+            </div>
+            <div class = "card-action">
+               <button class = "btn waves-effect waves-light blue-grey">
+               <i class = "material-icons">share</i></button>
+               <a class = "right blue-grey-text" href = "http://www.tutorialspoint.com">
+               www.tutorialspoint.com</a>
+            </div>
+         </div>
+      </div>
+      <div class = "col s4 m4">
+         <div class = "card blue-orange lighten-4">
+            <div class = "card-image">
+               <img src = "http://blog.safetec.com.br/wp-content/uploads/2017/03/como-fazer-planejamento-financeiro-1.jpg">                
+            </div>
+            <div class = "card-content">
+               <p>HTML5 is the next major revision of the HTML standard superseding
+                  HTML 4.01, XHTML 1.0, and XHTML 1.1. HTML5 is a standard for
+                  structuring and presenting content on the World Wide Web.
+               </p>
+            </div>
+            <div class = "card-action">
+               <button class = "btn waves-effect waves-light blue-grey">
+               <i class = "material-icons">share</i></button>
+               <a class = "right blue-grey-text" href = "http://www.tutorialspoint.com">
+               www.tutorialspoint.com</a>
+            </div>
+         </div>
+      </div>
+      <div class = "col s4 m4">
+         <div class = "card blue-orange lighten-4">
+            <div class = "card-image">
+               <img src = "http://blog.safetec.com.br/wp-content/uploads/2017/03/como-fazer-planejamento-financeiro-1.jpg">                
+            </div>
+            <div class = "card-content">
+               <p>HTML5 is the next major revision of the HTML standard superseding
+                  HTML 4.01, XHTML 1.0, and XHTML 1.1. HTML5 is a standard for
+                  structuring and presenting content on the World Wide Web.
+               </p>
+            </div>
+            <div class = "card-action">
+               <button class = "btn waves-effect waves-light blue-grey">
+               <i class = "material-icons">share</i></button>
+               <a class = "right blue-grey-text" href = "http://www.tutorialspoint.com">
+               www.tutorialspoint.com</a>
+            </div>
+         </div>
+      </div>
+      <div class="col s12 m4">
+         <div class="card-content white-text">
+            <div class="card orange accent-3">
                <div class="card-content white-text">
                   <p class="bounceEffect animated bounceIn">
                      <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                   </p>
                </div>
             </div>
-  
-           </div>
+         </div>
       </div>
-
-
-
-
-   <div class="col s12 m4">
-        <div class="card-content white-text">
-     
-  <div class="card orange accent-3">
+      <div class="col s12 m4">
+         <div class="card-content white-text">
+            <div class="card orange accent-3">
                <div class="card-content white-text">
                   <p class="bounceEffect animated bounceIn">
                      <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                   </p>
                </div>
             </div>
-  
-           </div>
+         </div>
       </div>
-
-
-   <div class="col s12 m4">
-        <div class="card-content white-text">
-     
-  <div class="card orange accent-3">
+      <div class="col s12 m4">
+         <div class="card-content white-text">
+            <div class="card orange accent-3">
                <div class="card-content white-text">
                   <p class="bounceEffect animated bounceIn">
                      <iframe width="100%" height="20%" src="https://www.youtube.com/embed/KuxLypSoIWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                   </p>
                </div>
             </div>
-  
-           </div>
+         </div>
       </div>
-
-     </div>
-                         </div>
-   
-   
-   
-   
+   </div>
+</div>
+<div id="appMenu" class="">
+   <ul>
+      <li>
+         <a href="http://gestao.fly01dev.com.br/" target="_blank">
+            <div class="app with-tooltip" id="gestao" data-tooltip="Fly01 Gestão" data-tooltip-id="umbib5g">
+               <div class="app-content white-text" style="background:#f37021;"><img class="app-icon loaded" src="https://cdnfly01.azureedge.net/img/icon/gestao.png" onload="$(this).addClass('loaded')"></div>
+            </div>
+         </a>
+      </li>
+      <li>
+         <a href="http://faturamento.fly01dev.com.br" target="_blank">
+            <div class="app with-tooltip" id="00ba2502-447e-4670-8885-b67d64496141" data-tooltip="Fly01 Faturamento" data-tooltip-id="4ze413h">
+               <div class="app-content white-text" style="background:#6f1200;"><img class="app-icon loaded" src="https://cdnfly01.blob.core.windows.net/img/icon/faturamento.png" onload="$(this).addClass('loaded')"></div>
+            </div>
+         </a>
+      </li>
+      <li>
+         <a href="http://compras.fly01dev.com.br" target="_blank">
+            <div class="app with-tooltip" id="65e3ba3e-6fcd-4527-b2a4-9dc37c80e285" data-tooltip="Fly01 Compras" data-tooltip-id="g5pb31h">
+               <div class="app-content white-text" style="background:#bf311a;"><img class="app-icon loaded" src="https://cdnfly01.blob.core.windows.net/img/icon/compras.png" onload="$(this).addClass('loaded')"></div>
+            </div>
+         </a>
+      </li>
+      <li>
+         <a href="http://estoque.fly01dev.com.br" target="_blank">
+            <div class="app with-tooltip" id="f259ea8b-f966-4c4f-8b18-97fa4bf2af87" data-tooltip="Fly01 Estoque" data-tooltip-id="far0sn1">
+               <div class="app-content white-text" style="background:#00b274;"><img class="app-icon loaded" src="https://cdnfly01.blob.core.windows.net/img/icon/estoque.png" onload="$(this).addClass('loaded')"></div>
+            </div>
+         </a>
+      </li>
+   </ul>
+</div>
 @endsection
 @section('footer')
 <footer class="page-footer black">
@@ -159,4 +166,3 @@
    </div>
 </footer>
 @endsection
-
