0b10ebf153ded404fd70984d299c6bc16fbc82d4
Lucas <lucas.c.victor@hotmail.com>
2018-04-05 08:46:10 -0300

    complemento para teste com valores aleatorios de DRE

--- app/Fly01/Classes/DRE.php
+++ app/Fly01/Classes/DRE.php
@@ -4,10 +4,21 @@ namespace App\Fly01\Classes;
 class DRE 
 {
     protected $categories;
-    
+
     public function __construct()
     {
+        $categories = array(
+            1 => array(
+                 "title" => "Receita de prestação de serviços",
+                 "type" => 1
+            ),
+            2 => array(
+                "title" => "Manutenção de equipamentos",
+                "type" => 2,
+                "origin" => 1
+            ),
 
+        );
     }
 
     public function sumAll($itens)
