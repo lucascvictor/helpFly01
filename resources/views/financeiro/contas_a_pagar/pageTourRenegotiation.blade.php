ae454f49c361e5775ada6ee49b3a0ac2cd653b91
Felipe Canova <fc.lavagnoli@gmail.com>
2018-04-17 09:21:47 -0300

    Correção tour renegotiation

--- resources/views/financeiro/contas_a_pagar/pageTourRenegotiation.blade.php
+++ resources/views/financeiro/contas_a_pagar/pageTourRenegotiation.blade.php
@@ -16,13 +16,13 @@
                     
                   
                     
-                <div class="card-panel teal amber accent-2 app center">Esta é a tela inicial da rotina, a seleção de títulos que serão renegociados, é pesquisada através dos fornecedores, por isso primeiramente é necessário selecioná-los, após isso clique em "Atualizar Contas" e os títulos em abertos do Fornecedor escolhido irá aparecer.</div>
+                <div class="card-panel teal amber accent-2 app center">Esta é a tela inicial da rotina, a seleção de títulos que serão renegociados, é pesquisada através dos fornecedores, por isso primeiramente é necessário selecioná-los, após isso clique em "Listar Contas" e os títulos em abertos do fornecedor escolhido irão aparecer.</div>
                     <img class="responsive-img z-depth-5" src="{{ url('images/cadastros/contasAPagar/Renegotiation/Tela1.gif') }}">
                    
-                    <div class="card-panel teal amber accent-2 app center">Vale ressaltar que após a lista de títulos aparecer, para selecionar os títulos  basta clicar em cima dos mesmos, podendo sempre escolher 1 ou mais títulos.</div>
+                    <div class="card-panel teal amber accent-2 app center">Vale ressaltar que após a lista de títulos aparecer, para selecionar os títulos  basta clicar em cima dos mesmos, podendo sempre escolher um ou mais títulos.</div>
                     <img class="responsive-img z-depth-5" src="{{ url('images/cadastros/contasAPagar/Renegotiation/Tela2.gif') }}">
                    
-                    <div class="card-panel teal amber accent-2 app center">Em "Informações da renegociação" preenchemos o motivo da renegociação, informamos se haverá acréscimo ou desconto,se a diferença será calculada diretamente por valor ou percentagem e por fim informamos o porcentual ou valor da diferença.</div>
+                    <div class="card-panel teal amber accent-2 app center">Em "Informações da renegociação" preenchemos o motivo da renegociação, informamos se haverá acréscimo ou desconto, se a diferença será calculada diretamente por valor ou porcentagem e por fim informamos o percentual ou valor da diferença.</div>
                     <img class="responsive-img" src="{{ url('images/cadastros/contasAPagar/Renegotiation/Tela3.png') }}">
                     
                     <div class="card-panel teal amber accent-2 app center">Já em "Informações da nova conta a pagar", é necessário entender que as informações inseridas nestes campos serão usadas para criação de um novo título.</div>
