ad3653d7ac315231a9f7a720c902c9dd6bc2e5fb
Lucas <lucas.c.victor@hotmail.com>
2018-04-18 08:36:48 -0300

    correção do active em menus de dia semana e mes

--- resources/views/financeiro/fluxo_de_caixa/index.blade.php
+++ resources/views/financeiro/fluxo_de_caixa/index.blade.php
@@ -36,9 +36,10 @@
         <div id="fly01btngrpField" class="input-field col s12 m6 l4">
             <label class="truncate active" for="fly01btngrp">Selecione o período</label>
             <div id="fly01btngrp" class="btn-group input-field">
-            <a id="btnDia" name="btnDia" class="btn col s4 @if() active @endif" href="{{ url('/financeiro/fluxodecaixa/day') }}"  >Dia</a>
-            <a id="btnSemana" name="btnSemana" class="btn col s4 @if() active @endif" href="{{ url('/financeiro/fluxodecaixa/week') }}" >Semana</a>
-            <a id="btnMes" name="btnMes" class="btn col s4 @if() active @endif" href="{{ url('/financeiro/fluxodecaixa') }}" >Mês</a>
+            <?php $pagina = $_SERVER['PHP_SELF'];?>
+            <a id="btnDia" name="btnDia" class="btn col s4 @if($pagina == '/index.php/financeiro/fluxodecaixa/day') active @endif" href="{{ url('/financeiro/fluxodecaixa/day') }}"  >Dia</a>
+            <a id="btnSemana" name="btnSemana" class="btn col s4 @if($pagina == '/index.php/financeiro/fluxodecaixa/week') active @endif" href="{{ url('/financeiro/fluxodecaixa/week') }}" >Semana</a>
+            <a id="btnMes" name="btnMes" class="btn col s4 @if($pagina == '/index.php/financeiro/fluxodecaixa/') active @endif" href="{{ url('/financeiro/fluxodecaixa') }}" >Mês</a>
             </div>
         </div>
         </form>
